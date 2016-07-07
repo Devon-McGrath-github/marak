d380d765f5f466faf185ef197f79d2303d23917f
Devon McGrath <devon.mcgrath@gmail.com>
2016-07-08 10:03:54 +1200

    fixed the browserHistory issue/created the structure for multiple reducers

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router'
 
 export default (props) => (
   <div>
-      <h1>Landing</h1>
-        <Link to='Events'><button>Event </button></Link>
-         <Link to='New-Event'><button>New Event </button></Link>      
+    <Link to='/'><h1>Landing</h1></Link>
+    <Link to='Events'><button>Event </button></Link>
+    <Link to='New-Event'><button>New Event </button></Link>
   </div>
 )
--- client/index.js
+++ client/index.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Link, hashHistory, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './components/App'
-import reducer from './reducer'
+import appReducer from './reducers'
 import Landing from './containers/LandingContainer'
-import EventList from './components/Landing'   //placeholder replace when exists
+import EventList from './containers/LandingContainer'   //placeholder replace when exists
 import CreateEvent from './components/Landing' //placeholder replace when exists
 
 let store = createStore(
-  reducer, compose(
+  appReducer, compose(
     applyMiddleware(),
     window.devToolsExtension ? window.devToolsExtension() : f => f
-
   )
 )
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Provider store={store}>
-      <Router history={browserHistory}>
+      <Router history={history}>
         <Route path="/" component={App}>
           <IndexRoute component={Landing} />
-          <Route path='Events' component= {EventList} />
+          <Route path='Events' component={EventList} />
           <Route path='New-Event' component={CreateEvent} />
         </Route>
       </Router>
