7643f529938feaf23fb11914f6970bef738c9c78
Devon McGrath <devon.mcgrath@gmail.com>
2016-07-08 09:35:27 +1200

    Added landing page with buttons/set up placeholder routes on index.js

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router'
 export default (props) => (
   <div>
       <h1>Landing</h1>
-      <p>{props.testerino}</p>
+        <Link to='Events'><button>Event </button></Link>
+         <Link to='New-Event'><button>New Event </button></Link>      
   </div>
 )
--- client/index.js
+++ client/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, Link, hashHistory, browserHistory, IndexRoute } from 'react-router'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './components/App'
 import reducer from './reducer'
 import Landing from './containers/LandingContainer'
+import EventList from './components/Landing'   //placeholder replace when exists
+import CreateEvent from './components/Landing' //placeholder replace when exists
 
 let store = createStore(
   reducer, compose(
@@ -19,9 +21,11 @@ let store = createStore(
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Provider store={store}>
-      <Router history={hashHistory}>
+      <Router history={browserHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={Landing} />
+          <Route path='Events' component= {EventList} />
+          <Route path='New-Event' component={CreateEvent} />
         </Route>
       </Router>
     </Provider>,
