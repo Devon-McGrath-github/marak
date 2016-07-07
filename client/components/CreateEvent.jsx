a1e192dd74f211a90408f918c715fd595df92e32
Devon McGrath <devon.mcgrath@gmail.com>
2016-07-08 10:30:01 +1200

    Edited url links to lowercase

--- client/components/CreateEvent.jsx
+++ client/components/CreateEvent.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router'
 export default (props) => (
   <div>
       <Link to='/'><h1>Home</h1></Link>
-    <Link to='Events'><button>Event </button></Link>
-    <Link to='New-Event'><button>New Event </button></Link>
+    
   </div>
 )
