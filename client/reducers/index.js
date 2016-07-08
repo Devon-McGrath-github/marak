977dc48baaa7d2cd990018c03a948f1b7dbb80fb
josh-jacobsen <joshjacobsen5@gmail.com>
2016-07-08 16:00:48 +1200

    Big changes to state (default.js INITIAL STATE)
    getReducer is now the big state which will have all our information, default is now a dead file, can be used/changed if we need another state that should be default, or the current changes can easily be refactored back into default.js

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,11 +3,13 @@ import { routerReducer } from 'react-router-redux'
 import {reducer as formReducer} from 'redux-form'
 
 import defaultReducer from './default'  // placeholder reducer
+import getReducer from './getReducer'
 
 const reducers = {
   routing: routerReducer,
   form: formReducer,
-  defaultReducer
+  defaultReducer,
+  getReducer
 }
 
 const appReducer = combineReducers(reducers)
