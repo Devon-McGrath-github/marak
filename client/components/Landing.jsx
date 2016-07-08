c786097ed4d8370a3fb0cf18932d67ec39b7c2b8
josh-jacobsen <joshjacobsen5@gmail.com>
2016-07-08 15:54:52 +1200

    removed testerino data from containers

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -4,8 +4,12 @@ import { Link } from 'react-router'
 export default (props) => (
   <div>
     <Link to='/'><h1>Home</h1></Link>
-    <Link to='events'><button>Event </button></Link>
+    <Link to='event'><button>Event </button></Link>
     <Link to='new-event'><button>New Event </button></Link>
     <Link to='event-details'><button>Event details</button></Link>
+
+    <div id='reducer-test-div'>
+
+    </div>
   </div>
 )
