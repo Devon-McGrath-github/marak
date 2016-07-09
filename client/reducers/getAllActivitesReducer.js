210db7641c76d097ad29c178bc5e4ce7ec1c67f4
Devon McGrath <devon.mcgrath@gmail.com>
2016-07-09 16:59:54 +1200

    removed initialState.js

--- client/reducers/getAllActivitesReducer.js
+++ client/reducers/getAllActivitesReducer.js
@@ -1,9 +1,6 @@
-import data from './initialState'
 import {RECIEVE_ACTIVITIES} from '../actions/getAllActivitiesAction'
 
-const INITIAL_STATE = data
-
-const reducer = (state = INITIAL_STATE , action) => {
+const reducer = (state = {} , action) => {
   switch (action.type) {
     case RECIEVE_ACTIVITIES:
       return action.activities
