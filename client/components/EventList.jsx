800d274b9d6d5c6f643ce9e86253eddd2aba34fb
Abhi <abhi.kala123@gmail.com>
2016-07-09 15:51:55 +1200

    refactor to iterate over objects with Ramda

--- client/components/EventList.jsx
+++ client/components/EventList.jsx
@@ -1,40 +1,27 @@
 import React from 'react'
+import R from 'ramda'
 import { Link } from 'react-router'
 
-// export default React.createClass({
-//   propTypes: {
-//     activities: React.PropTypes.array
-//   },
-//   render() {
-//     console.log(this.props.activities);
-//     return (
-//       <div>
-//         <Link to='/'><h1>Home</h1></Link>
-//         <h1>Event List</h1>
-//           {this.props.activities.map(function (activity) {
-//             return (
-//               <div key={activity.id}>
-//               <p>Title: {activity.title}</p>
-//               <p>subtitle: {activity.subtitle}</p>
-//               <p>Date: {activity.date}</p>
-//               <Link to={`/event-details/${activity.id}`}><button>View Details</button></Link>
-//             </div>
-//           )
-//
-//           })}
-//       </div>
-//     )
-//   }
-// })
-
 export default (props) => {
-  const activity = props.activities
-  // console.log("activity", activity);
+  const activities = R.values(props.activities)
+  console.log("activity", activities);
   return (
     <div>
     <h1>Events List</h1>
-    <h4>{activity.description}</h4>
-
+      {activities.map(function (activity) {
+          console.log(activity);
+          return (
+            <div key={activity.id}>
+            <p>Title: {activity.title}</p>
+            <p>subtitle: {activity.subtitle}</p>
+            <p>Date: {activity.date}</p>
+            <Link to={`/event-details/${activity.id}`}><button>View Details</button></Link>
+            </div>
+          )
+          })
+        }
   </div>
 )
 }
+
+// <h4>{activity.activity1.description}</h4>
