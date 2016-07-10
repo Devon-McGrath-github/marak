04af27af635265bcbca17698d53194c37e9ab1a3
Devon McGrath <devon.mcgrath>
2016-07-11 11:27:57 +1200

    some refactoring

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -6,8 +6,10 @@ export default (props) => (
     <Link to='/'><h1>Home</h1></Link>
     <Link to='event-list'><button onClick={() => props.getActivities()}>Event List</button></Link>
     <Link to='new-event'><button>New Event </button></Link>
-    <button onClick={() => props.getUserInfo()}>test</button>
-    
+
+    {/* This needs to be attached to the sign in button so we get the currentUserId, when their is a currentUser */}
+    <button onClick={() => props.getUserInfo()}>getUserInfo button</button>
+
     <div id='reducer-test-div'>
 
     </div>
