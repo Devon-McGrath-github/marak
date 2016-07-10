9d392adc80ab43b5ab6e832d172f5a69d8813819
Ricky-Kynd <Ricky-Kynd@gmail.com>
2016-07-10 19:33:44 +1200

    working auth but not working with databse yet

--- client/firebaseInit.js
+++ client/firebaseInit.js
@@ -63,10 +63,10 @@ var uiConfig = {
           'signInSuccessUrl': '/',
           'signInOptions': [
             // Leave the lines as is for the providers you want to offer your users.
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+            // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            // firebase.auth.GoogleAuthProvider.PROVIDER_ID
           ],
           // Terms of service url.
           'tosUrl': '<your-tos-url>',
