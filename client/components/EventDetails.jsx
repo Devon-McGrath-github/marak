7f96e527d5b980d2e79baf4f4aa6425642392d52
Devon McGrath <devon.mcgrath@gmail.com>
2016-07-11 15:54:35 +1200

    Toggle RSVP working

--- client/components/EventDetails.jsx
+++ client/components/EventDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import R from 'ramda'
 
 export default React.createClass({
   propTypes: {
@@ -8,12 +9,13 @@ export default React.createClass({
   render () {
     let activity = this.props.activity
     let activityId = activity ? activity.activityId : 0
-    let attendeeId = 10089
+    let currentUserId = 10089
 
     let activityCreatorId = 56
 
-    let currentUserId = 56
+    let array = []
 
+    console.log(activity)
     if (activity) {
       return (
         <div>
@@ -24,10 +26,19 @@ export default React.createClass({
           <p>Description: {activity.description}</p>
           <p>Date: {activity.activityStart && activity.activityEnd}</p>
           <p>Location: {activity.formattedAddress}</p>
-          <p>Tasks: {activity.tasks}</p>        
+          <p>Tasks: {activity.tasks}</p>
           <p>Number people attending: {this.props.length} / {activity.numberRequired} </p>
 
-          <button onClick={() => { this.props.toggleRSVP(attendeeId, activityId)} }>RSVP</button>
+          <button onClick={() => { this.props.toggleRSVP(currentUserId, activityId)} }>
+            {R.values(activity.attendeeIds).map((attendeeId) => {
+              if (attendeeId === currentUserId) {
+                array.push(attendeeId)
+              }
+            }),
+              console.log(array)
+            /* attendeeId === currentUserId ? 'RSVP' : 'Cancel RSVP' */
+          } { array.length == 1 ? 'RSVP' : 'Cancel RSVP' }
+          </button>
           <Link to='event-list'><button onClick={() => { this.props.deleteActivity(currentUserId, activityCreatorId, activityId)} }>Delete Event</button></Link>
         </div>
       )
