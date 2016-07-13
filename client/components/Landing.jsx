580ea276bcb6a1b9500eff60d025a5535a9bc016
Devon McGrath <devon.mcgrath@gmail.com>
2016-07-13 23:46:55 +1200

    before midnight

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -14,13 +14,7 @@ export default (props) => {
             <h1 className="hero-h1">MARAK</h1>
           </div>
           <div className="hero-description">
-              <h3 className="hero-h3">Bacon ipsum dolor amet
-                  bresaola ground round alcatra ball tip,
-                  meatloaf spare ribs short loin doner shank.
-                  Turkey bresaola pig leberkas. Tail pork belly
-                  t-bone brisket, corned beef alcatra ground
-                  round sirloin kielbasa. Pork loin tail
-                  bacon cupim cow.</h3>
+              <h3 className="hero-h3">Connect with others </h3>
           </div>
           <div className="hero-button">
             <Link to='event-list'><button className="ui inverted black button"><span className="events-list-text">Events Near Me</span></button></Link>
